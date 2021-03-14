c610a20e19bc41f0e9bcd52ccfc46004730b8c10
Ndoye.Momar <momar.ndoye@etu.univ-orleans.fr>
2021-03-14 17:37:29 +0100

    Commit theme

---
+++ miage-angular/src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+
+
+  constructor(private users: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+}
