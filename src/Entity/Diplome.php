acb475a7fe7fb39fc4d60bab2865cf5f945cd267
Dénez Fauchon <denezfauchon@gmail.com>
2021-03-16 22:01:11 +0000

    Revert "Merge branch 'revert-5205c743' into 'master'"
    This reverts merge request !12

--- src/Entity/Diplome.php
+++ src/Entity/Diplome.php
@@ -6,11 +6,9 @@ use App\Repository\DiplomeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=DiplomeRepository::class)
- * @ApiResource()
  */
 class Diplome
 {
